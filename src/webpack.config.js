7d03943feea3509317d5a80e18e9889d6bb6a9bc
Tomo-kw <mulberryfield412@yahoo.ne.jp>
2022-10-08 22:54:53 +0900

    webpack関連ファイル生成、package.jsonにモード指定コマンド追加

---
+++ src/webpack.config.js
@@ -0,0 +1,15 @@
+/** ↓ エディタで補完を効かせるための JSDoc */
+/** @type {import('webpack').Configuration} */
+module.exports = {
+    mode: "development",
+    module: {
+      rules: [
+        {
+          // 拡張子 js のファイル（正規表現）
+          test: /\.js$/,
+          // ローダーの指定
+          loader: "babel-loader",
+        },
+      ],
+    },
+  };
